42bcf43ad85b069768647cf51eeeb5dfdfddcbc8
kargo <karenrgo@gmail.com>
2019-08-28 11:49:26 -0700

    set up index/show pages for capsules and items

--- app/controllers/api/v1/capsules_controller.rb
+++ app/controllers/api/v1/capsules_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::CapsulesController < ApplicationController
+  
+  def index
+    capsules = Capsule.all
+    render json: capsules
+  end
+
+  def show
+    capsule = Capsule.find(params[:id])
+    render json: capsule
+  end
+
 end
