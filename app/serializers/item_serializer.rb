26087d0ce93b5e6aab3915b7ca5984ce01694eef
kargo <karenrgo@gmail.com>
2019-09-09 16:33:04 -0700

    added capsule serializer to result of update item response

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,3 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :brand, :shop_link, :color, :category, :category2, :price, :image
+  attributes :id, :name, :brand, :shop_link, :description, :color, :category, :category2, :price, :image, :personal
 end
