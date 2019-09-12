c67bcd46c19b61484c9ce1f88247811c026244ed
kargo <karenrgo@gmail.com>
2019-09-11 19:06:52 -0700

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'capsule-curate-client.herokuapp.com/'
+    origins 'https://capsule-curate-client.herokuapp.com'
 
     resource '*',
       headers: :any,
