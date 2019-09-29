e8f6294265c170a3d2d54b0f1d1bc878d17488d1
kargo <karenrgo@gmail.com>
2019-09-29 15:10:37 -0700

    nordy scraper

--- db/nordstrom.rb
+++ db/nordstrom.rb
@@ -15,7 +15,6 @@ def nordy(url, category, category2)
 
   collection.each do |i|
     
-    img_div = i.children[0]
     img_url = i.children[0].children[0].attributes['src'].value
     shop_link = i.children[0].children[1].attributes['href'].value
 
@@ -27,20 +26,23 @@ def nordy(url, category, category2)
 
     # color swatches ... sometimes does not exist
     # colors = i.children[1].css('li') 
+    binding.pry
 
-    Item.find_or_create_by(shop_link: shop_link) do |item|
-      item.name = name
-      item.price = price
-      item.brand = brand
-      item.color = color
-      item.category = category
-      item.category2 = category2
-      item.image = img_url
-    end
+    # item = Item.find_or_create_by(shop_link: shop_link)
+    # item.name = name
+    # item.price = price
+    # item.brand = brand
+    # item.color = color
+    # item.category = category
+    # item.category2 = category2
+    # item.image = img_url
 
   end
 
+  if collection.count == 52
+    next_page = obj.css('span._3xquK')
+  end
 
 end
 
-nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_60202026', 'outerwear', 'outerwear')
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_8000819', 'outerwear', 'outerwear')
--- db/seeds.rb
+++ db/seeds.rb
@@ -45,11 +45,64 @@ def everlane(file, category, category2)
 
 end
 
-everlane('db/everlane/sweaters.html','sweater','top')
-everlane('db/everlane/denim.html','denim','bottoms')
-everlane('db/everlane/dresses.html','dress','one piece')
-everlane('db/everlane/outerwear.html','outerwear','outerwear')
-everlane('db/everlane/pants.html','pants','bottoms')
-everlane('db/everlane/shorts.html','shorts','bottoms')
-everlane('db/everlane/tees.html','tee','top')
-everlane('db/everlane/tops.html','top','top')
+# everlane('db/everlane/sweaters.html','sweater','top')
+# everlane('db/everlane/denim.html','denim','bottoms')
+# everlane('db/everlane/dresses.html','dress','one piece')
+# everlane('db/everlane/outerwear.html','outerwear','outerwear')
+# everlane('db/everlane/pants.html','pants','bottoms')
+# everlane('db/everlane/shorts.html','shorts','bottoms')
+# everlane('db/everlane/tees.html','tee','top')
+# everlane('db/everlane/tops.html','top','top')
+
+
+def nordy(url, category, category2)
+  base_url = 'https://shop.nordstrom.com'
+  brand = 'Nordstrom Sustainable Style'
+  html_data = open(url).read
+  # html_data = File.read(file)
+  obj = Nokogiri::HTML(html_data)
+  
+  collection = obj.css("article._1AOd3")
+  # item_div_class = '_1AOd3'
+
+  collection.each do |i|
+    img_url = i.children[0].children[0].attributes['src'].value
+    shop_link = base_url + i.children[0].children[1].attributes['href'].value
+
+    name = i.css('h3').children[0].children[0].text
+    price = i.css('span._3wu-9').text
+    
+    # parse color from shop_link
+    color = shop_link[shop_link.index('color=')+6..-1].gsub('%20', ' ')
+    # color swatches ... sometimes does not exist
+    # colors = i.children[1].css('li') 
+
+    item = Item.find_or_create_by(shop_link: shop_link)
+    item.name = name
+    item.price = price
+    item.brand = brand
+    item.color = color
+    item.category = category
+    item.category2 = category2
+    item.image = img_url
+    item.save
+    puts item
+  end
+
+end
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_60202026', 'outerwear', 'outerwear')
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_60130139', 'dress', 'one piece')
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_8000796', 'outerwear', 'outerwear')
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_8000819', 'denim', 'bottoms')
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_8000831', 'pants', 'bottoms')
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_60129453', 'top', 'top')
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_8000851', 'sweater', 'top')
+
+nordy('https://shop.nordstrom.com/c/sustainable-style?breadcrumb=Home%2FSustainable%20Style&flexi=60126782_60126784&flexi=8000786_60129871', 'tee', 'top')
